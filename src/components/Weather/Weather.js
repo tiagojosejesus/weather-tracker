0d97a3ec8dcfc58a3cec2c394bea926446bf04f5
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-15 17:22:42 +0000

    Added style to Weather.js

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,29 +1,59 @@
 import React from "react";
+import "./Weather.scss";
 
 const Weather = ({ weatherData }) => {
   console.log(weatherData);
   return (
-    <div>
+    <div className="container">
       {weatherData.map((weather) => (
-        <div>
+        <>
           {weather.loading || !weather.data ? (
-            <p>loading...</p>
+            <></>
           ) : (
-            <div>
-              {weather.data.forecast.forecastday.map((days) => (
-                <div className="weather-container">
-                  <img src={days.day.condition.icon} alt=""></img>
-                  <p>Date: {days.date}</p>
-                  <p>Minimum Temperature: {days.day.mintemp_c} ºC</p>
-                  <p>Maximum Temperature: {days.day.maxtemp_c} ºC</p>
-                  <p>Chance of Rain: {days.day.daily_chance_of_rain} %</p>
-                  <p>Precipitation: {days.day.totalprecip_mm} mm</p>
-                  <p></p>
-                </div>
-              ))}
-            </div>
+            <>
+              <div>
+                {weather.data.forecast.forecastday.map((days) => (
+                  <div className="weather-container">
+                    <img src={days.day.condition.icon} alt=""></img>
+                    <div className="date">Date: {days.date}</div>
+                    <div className="info">
+                      Minimum Temperature: {days.day.mintemp_c} ºC
+                    </div>
+                    <div className="info">
+                      Maximum Temperature: {days.day.maxtemp_c} ºC
+                    </div>
+                    <div className="info">
+                      Chance of Rain: {days.day.daily_chance_of_rain} %
+                    </div>
+                    <div className="info">
+                      Precipitation: {days.day.totalprecip_mm} mm
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div>
+                {weather.data.forecast.forecastday.map((days) => (
+                  <div className="weather-container">
+                    <img src={days.day.condition.icon} alt=""></img>
+                    <div className="date">Date: {days.date}</div>
+                    <div className="info">
+                      Minimum Temperature: {days.day.mintemp_c} ºC
+                    </div>
+                    <div className="info">
+                      Maximum Temperature: {days.day.maxtemp_c} ºC
+                    </div>
+                    <div className="info">
+                      Chance of Rain: {days.day.daily_chance_of_rain} %
+                    </div>
+                    <div className="info">
+                      Precipitation: {days.day.totalprecip_mm} mm
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
           )}
-        </div>
+        </>
       ))}
     </div>
   );
