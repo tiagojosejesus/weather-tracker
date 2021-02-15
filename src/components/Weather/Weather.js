739406a241bcb71e097edeccfef81df1c243bf21
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-15 23:06:27 +0000

    Removed test code

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -11,46 +11,24 @@ const Weather = ({ weatherData }) => {
             <></>
           ) : (
             <>
-              <div>
-                {weather.data.forecast.forecastday.map((days) => (
-                  <div className="weather-container">
-                    <img src={days.day.condition.icon} alt=""></img>
-                    <div className="date">Date: {days.date}</div>
-                    <div className="info">
-                      Minimum Temperature: {days.day.mintemp_c} ºC
-                    </div>
-                    <div className="info">
-                      Maximum Temperature: {days.day.maxtemp_c} ºC
-                    </div>
-                    <div className="info">
-                      Chance of Rain: {days.day.daily_chance_of_rain} %
-                    </div>
-                    <div className="info">
-                      Precipitation: {days.day.totalprecip_mm} mm
-                    </div>
+              {weather.data.forecast.forecastday.map((days) => (
+                <div className="weather-container">
+                  <img src={days.day.condition.icon} alt=""></img>
+                  <div className="date">Date: {days.date}</div>
+                  <div className="info">
+                    Minimum Temperature: {days.day.mintemp_c} ºC
                   </div>
-                ))}
-              </div>
-              <div>
-                {weather.data.forecast.forecastday.map((days) => (
-                  <div className="weather-container">
-                    <img src={days.day.condition.icon} alt=""></img>
-                    <div className="date">Date: {days.date}</div>
-                    <div className="info">
-                      Minimum Temperature: {days.day.mintemp_c} ºC
-                    </div>
-                    <div className="info">
-                      Maximum Temperature: {days.day.maxtemp_c} ºC
-                    </div>
-                    <div className="info">
-                      Chance of Rain: {days.day.daily_chance_of_rain} %
-                    </div>
-                    <div className="info">
-                      Precipitation: {days.day.totalprecip_mm} mm
-                    </div>
+                  <div className="info">
+                    Maximum Temperature: {days.day.maxtemp_c} ºC
                   </div>
-                ))}
-              </div>
+                  <div className="info">
+                    Chance of Rain: {days.day.daily_chance_of_rain} %
+                  </div>
+                  <div className="info">
+                    Precipitation: {days.day.totalprecip_mm} mm
+                  </div>
+                </div>
+              ))}
             </>
           )}
         </>
